c8fd3f627d4e477a31e3931fb5efae25da648f30
Lukasz <lukasz92626@gmail.com>
2026-06-20 12:40:52 +0200

    Fix preprocessing.

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -2,6 +2,7 @@ import re
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
+from bs4 import BeautifulSoup
 
 # Pobranie niezbędnych zasobów NLTK
 nltk.download('stopwords')
@@ -13,14 +14,24 @@ lemmatizer = WordNetLemmatizer()
 
 def preprocess_text(text):
     """Funkcja czyszcząca i normalizująca tekst recenzji"""
+    # Usuwanie znacznikow html
+    soup = BeautifulSoup(text, 'html.parser')
+    text = soup.get_text(separator=' ')
+
+    # Zamiana na małe litery
     text = text.lower()
 
+    # Usuwanie wszystkiego poza literami
     text = re.sub(r'[^a-zA-Z]', ' ', text)
 
+    # Podział na słowa
     words = text.split()
 
+    # Usuwanie stopwords
     words = [w for w in words if w not in stop_words]
 
+    # Lematyzacja
     words = [lemmatizer.lemmatize(w) for w in words]
 
+    # Połączenie z powrotem w tekst
     return " ".join(words)
